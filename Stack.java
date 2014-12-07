003ae776677099e64434f110fb074374d3312f4d
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2014-12-07 17:44:43 +0000

    initial version of Stack

---
+++ Stack.java
@@ -0,0 +1,70 @@
+package day7;
+
+public class Stack {
+
+	Request bottom;
+	
+	
+	public Stack () {
+		bottom =null;
+	}
+	
+	
+	public void push (Request req) {
+		
+		Request temp = bottom;
+		
+		if (bottom == null) {
+			bottom = req;
+		} else {
+			
+			while (temp.getNext()!=null) {
+				temp = temp.getNext();
+			}
+			
+			temp.setNext(req);
+			
+		}
+			
+	}
+	
+	public boolean pop () {
+		
+		Request temp = bottom;
+		
+		if (isEmpty()) {
+			System.out.println("The Stack is empty");
+			return false;
+		} else if (temp.getNext() == null){ 
+			
+			System.out.println("Popping... it’s a "+bottom.getRequestId());
+			bottom =null;
+			return true;
+			
+		}else {
+			
+			while ((temp.getNext().getNext()!= null)) {
+				temp = temp.getNext();
+			}
+			
+			System.out.println("Popping... it’s a "+temp.getNext().getRequestId());
+			temp.setNext(null);
+			return true;
+			
+		}
+
+	}
+
+
+
+	
+	
+	public boolean isEmpty () {
+		
+		if (bottom == null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
---
+++ StackScript.java
@@ -0,0 +1,41 @@
+package day7;
+
+public class StackScript {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Stack stack = new Stack();
+		
+		System.out.println("Pushing 5...");
+		stack.push(new Request(5));
+		
+		System.out.println("Pushing 8...");
+		stack.push(new Request(8));
+		
+		System.out.println("Pushing 12...");
+		stack.push(new Request(12));
+       
+		stack.pop();
+        
+		System.out.println("Pushing 13...");
+		stack.push(new Request(13));
+
+		stack.pop();
+		stack.pop();
+		stack.pop();
+		stack.pop();
+		
+		System.out.println("Pushing 5...");
+		stack.push(new Request(5));
+		System.out.println("Pushing 5...");
+		stack.push(new Request(5));
+		
+	
+		
+	}
+
+}
